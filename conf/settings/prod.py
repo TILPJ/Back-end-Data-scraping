e3352269e827f427000e06e69b31e86872084410
donhwi94 <ehsgnl123@naver.com>
2021-08-03 00:04:15 +0900

    heroku 배포를 위한 whitenoise 설정 추가

--- conf/settings/prod.py
+++ conf/settings/prod.py
@@ -6,12 +6,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["tilup-release-v1.herokuapp.com"]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = []
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
+
 
 # DATABASES = {
 #     "default": {
