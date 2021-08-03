f34c02320b80ce9a64eed7e13c10e93d8c39f432
donhwi94 <ehsgnl123@naver.com>
2021-08-03 14:01:39 +0900

    주석 제거, api release v1.0 배포

--- conf/settings/prod.py
+++ conf/settings/prod.py
@@ -6,26 +6,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["tilup-release-v1.herokuapp.com"]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = []
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("PROD_DATABASE_ENGINE"),
-#         "NAME": os.getenv("PROD_DATABASE_NAME"),
-#         "USER": os.getenv("PROD_DATABASE_USER"),
-#         "PASSWORD": os.getenv("PROD_DATABASE_PASSWORD"),
-#         "HOST": os.getenv("PROD_DATABASE_HOST"),
-#         "PORT": os.getenv("PROD_DATABASE_PORT"),
-#     }
-# }
