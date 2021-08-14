90d7424ea1619ccfc86b7c268e31b4c2dc5d214c
donhwi94 <ehsgnl123@naver.com>
2021-08-15 01:44:58 +0900

    유저별 학습 카드 중복 등록 오류 수정

--- courses/views.py
+++ courses/views.py
@@ -114,7 +114,7 @@ class MyCourseList(GenericAPIView):
         site_id = request.data.get("site")
         course_id = request.data.get("course")
         # 학습 카드 중복 등록 제거
-        if MyCourse.objects.filter(site_id=site_id, course_id=course_id):
+        if MyCourse.objects.filter(owner=request.user, site_id=site_id, course_id=course_id):
             res = jsend.fail(data={"detail": _("This is already registered.")})
             return Response(res)
 
@@ -157,7 +157,7 @@ class MyCourseDetail(GenericAPIView):
         site_id = request.data.get("site")
         course_id = request.data.get("course")
         # 학습 카드 중복 등록 제거
-        if MyCourse.objects.filter(site_id=site_id, course_id=course_id):
+        if MyCourse.objects.filter(owner=request.user, site_id=site_id, course_id=course_id):
             res = jsend.fail(data={"detail": _("This is already registered.")})
             return Response(res)
 
